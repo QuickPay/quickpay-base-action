7f8f4d0873b0cead8f6c44b50e3a2e6b01abb58b
Alexandra Østermark <alex.cramt@gmail.com>
2021-09-10 11:26:26 +0200

    added support for postgres

--- dist/index.js
+++ dist/index.js
@@ -14,11 +14,12 @@ async function run() {
     const prodAptDeps = core.getBooleanInput("prod_apt_deps", { required: false })
     const chrome = core.getBooleanInput("chrome", { required: false })
     const rubocop = core.getBooleanInput("rubocop", { required: false })
+    const postgres = core.getBooleanInput("postgres", { required: false })
     if (chrome) {
       cp.execSync("wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - ")
       cp.execSync(`sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'`)
     }
-    if (chrome || prodAptDeps) {
+    if (chrome || prodAptDeps || postgres) {
       cp.execSync("sudo apt-get update")
       const aptDeps = (chrome ? ["chromium-chromedriver", "google-chrome-stable"] : [])
         .concat(prodAptDeps ? [
@@ -28,7 +29,9 @@ async function run() {
           "libxslt1-dev",
           "zlib1g-dev",
           "netcat-openbsd",
-          "libsasl2-dev"] : []).join(" ")
+          "libsasl2-dev"] : [])
+        .concat(postgres ? ["postgresql-client"] : [])
+        .join(" ")
       cp.execSync("sudo apt-get install -y " + aptDeps)
     }
     if (sshKey) {
@@ -39,12 +42,29 @@ async function run() {
     }
     if (rubocop) {
       cp.execSync("curl -o ./.rubocop.yml https://quickpay.github.io/development/.rubocop.yml")
-    }
 
+    }
+    if (postgres) {
+      let i;
+      for (i = 0; i <= 60; i++) {
+        const result = cp.execSync(`echo "select pg_is_in_recovery()" | psql -t`).toString().trim()
+        await wait(1000)
+        if (result === "f") {
+          break
+        }
+      }
+      if (i == 60) {
+        core.setFailed("postgresql database timed out")
+        return
+      }
+      cp.execSync(`psql -d postgresql://backends_u@localhost/backends_test -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto";'`)
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
+
+const wait = (miliseconds) => new Promise((resolve, _) => setTimeout(resolve, miliseconds))
 
 run();
 
