19107b7aaeccf50b39c31aa9c6d4a3d86d31afd1
Alexandra Østermark <alex.cramt@gmail.com>
2021-09-16 12:30:27 +0200

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const cp = require("child_process")
+const core = require('@actions/core')
+const exec = require("@actions/exec")
 
 const getBool = (key) => core.getBooleanInput(key, { required: false })
 
@@ -15,12 +15,29 @@ async function run() {
     const postgres = getBool("postgres")
 
     if (chrome) {
-      cp.execSync("wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - ")
-      cp.execSync(`sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'`)
+      exec.exec(`sudo sh -c 'echo "deb http://deb.debian.org/debian buster main
+deb http://deb.debian.org/debian buster-updates main
+deb http://deb.debian.org/debian-security buster/updates main" > /etc/apt/sources.list.d/debian.list'`
+      )
+      exec.exec(`sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517 && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138 && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50 && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A`)
+      exec.exec(`sudo sh -c 'echo "# Note: 2 blank lines are required between entries
+Package: *
+Pin: release a=eoan
+Pin-Priority: 500
+
+Package: *
+Pin: origin "ftp.debian.org"
+Pin-Priority: 300
+
+# Pattern includes 'chromium', 'chromium-browser' and similarly
+# named dependencies:
+Package: chromium*
+Pin: origin "ftp.debian.org"
+Pin-Priority: 700" > /etc/apt/preferences.d/chromium.pref'`)
     }
     if (chrome || prodAptDeps || postgres) {
-      cp.execSync("sudo apt-get update")
-      const aptDeps = (chrome ? ["chromium-chromedriver", "google-chrome-stable"] : [])
+      exec.exec("sudo apt-get update")
+      const aptDeps = (chrome ? ["chromium-chromedriver", "chromium"] : [])
         .concat(prodAptDeps ? [
           "libpq-dev",
           "libcurl4-openssl-dev",
@@ -31,20 +48,20 @@ async function run() {
           "libsasl2-dev"] : [])
         .concat(postgres ? ["postgresql-client"] : [])
         .join(" ")
-      cp.execSync("sudo apt-get install -y " + aptDeps)
+      exec.exec("sudo apt-get install -y " + aptDeps)
     }
     if (sshKey) {
-      cp.execSync("mkdir ~/.ssh")
-      cp.execSync(`echo "${sshKey}" > ~/.ssh/id_ed25519`)
-      cp.execSync("ssh-keygen -F github.com || ssh-keyscan github.com >> ~/.ssh/known_hosts")
-      cp.execSync("chmod 600 ~/.ssh/id_ed25519")
+      exec.exec("mkdir ~/.ssh")
+      exec.exec(`echo "${sshKey}" > ~/.ssh/id_ed25519`)
+      exec.exec("ssh-keygen -F github.com || ssh-keyscan github.com >> ~/.ssh/known_hosts")
+      exec.exec("chmod 600 ~/.ssh/id_ed25519")
     }
     if (gemServer) {
-      cp.execSync("mkdir ~/.bundle")
-      cp.execSync(`echo "BUNDLE_GEMS__QUICKPAY__NET: \"${gemServer}\"" > ~/.bundle/config`)
+      exec.exec("mkdir ~/.bundle")
+      exec.exec(`echo "BUNDLE_GEMS__QUICKPAY__NET: \"${gemServer}\"" > ~/.bundle/config`)
     }
     if (rubocop) {
-      cp.execSync("curl -o ./.rubocop.yml https://quickpay.github.io/development/.rubocop.yml")
+      exec.exec("curl -o ./.rubocop.yml https://quickpay.github.io/development/.rubocop.yml")
 
     }
     if (postgres) {
@@ -54,7 +71,7 @@ async function run() {
       const connectionString = `postgresql://${user}:${password}@localhost/${db}`
       let i;
       for (i = 0; i <= 60; i++) {
-        const result = cp.execSync(`echo "select pg_is_in_recovery()" | psql -t -d ${connectionString}`).toString().trim()
+        const result = exec.exec(`echo "select pg_is_in_recovery()" | psql -t -d ${connectionString}`).toString().trim()
         await wait(1000)
         if (result === "f") {
           break
@@ -64,7 +81,7 @@ async function run() {
         core.setFailed("postgresql database timed out")
         return
       }
-      cp.execSync(`psql -d ${connectionString} -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto";'`)
+      exec.exec(`psql -d ${connectionString} -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto";'`)
     }
   } catch (error) {
     core.setFailed(error.message);
