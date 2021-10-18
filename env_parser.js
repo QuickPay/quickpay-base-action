3ae22c6e513fceea01f2629e0640ed8879491348
Alexandra Østemark <alex.cramt@gmail.com>
2021-10-18 09:18:28 +0200

    better .env parsing

--- env_parser.js
+++ env_parser.js
@@ -1,11 +1,15 @@
 //if you need to change this code, plz go check if the dotenv library supports multiline values between double qoutes and use that instead
-const regex = /(?<key>[a-zA-Z0-9_]+)=(?:(?<!\\)(?:"(?<value2>(?:.|\n)*?)(?<!\\)")|'?(?<value1>.*)'?)/gm
+const regex = /(?<key>[a-zA-Z0-9_]+)=(?<value>(?<!\\)(?:"(?:(?:.|\n)*?)(?<!\\)")|'?(?:.*)'?)/gm
 
 export const parse = (val) => {
     let matches = val.matchAll(regex);
     let obj = {}
     for(const match of matches){
-        obj[match.groups.key] = (match.groups.value2 || match.groups.value1).trim()
+        let result = match.groups.value.trim()
+        if(result[0] === '"' || result[0] === "'") {
+            result = JSON.parse(result)
+        }
+        obj[match.groups.key] = result
     }
     return obj
 }
