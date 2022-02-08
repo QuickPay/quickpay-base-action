c714ce94f333ab70912b1300acb304579bd58a57
Alexandra Østemark <alex.cramt@gmail.com>
2022-02-08 12:26:02 +0100

    add tests

--- integration_test/test/postgres_connection_spec.rb
+++ integration_test/test/postgres_connection_spec.rb
@@ -10,4 +10,8 @@ describe "Postgres Connection" do
     it "has pgcrypto installed" do
         _(`psql -S -d postgresql://backends_u:abc@localhost/backends_test -c "select gen_random_uuid();"`).must_match /gen_random_uuid/
     end
+
+    it "has generate_ulid installed" do
+        _(`psql -S -d postgresql://backends_u:abc@localhost/backends_test -c "select generate_ulid();"`).must_match /generate_ulid/
+    end
 end
